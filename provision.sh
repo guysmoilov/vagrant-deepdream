1dc82899db5f6d671236a357c39f2193a188ad8f
Guy Smoilovsky <guy.smoilovsky@hp.com>
2015-07-17 00:46:38 +0300

    Added force flag to provision script

--- provision.sh
+++ provision.sh
@@ -1,6 +1,6 @@
 mkdir -p /home/vagrant/sasi
-cp /vagrant/deepdream.ipynb /home/vagrant/sasi/ 
+cp -f /vagrant/deepdream.ipynb /home/vagrant/sasi/ 
 chown -R vagrant:vagrant /home/vagrant
-cp /vagrant/deepdream.conf /etc/init/
+cp -f /vagrant/deepdream.conf /etc/init/
 stop deepdream
 start deepdream
