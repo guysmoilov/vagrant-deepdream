0eead5306ba87f1542a4a046d35113cccdbbd650
Guy Smoilovsky <guy.smoilovsky@hp.com>
2015-07-23 11:13:52 +0300

    Provision script logging + crlf crap

--- provision.sh
+++ provision.sh
@@ -1,6 +1,7 @@
-mkdir -p /home/vagrant/sasi
-cp -f /vagrant/deepdream.ipynb /home/vagrant/sasi/ 
-chown -R vagrant:vagrant /home/vagrant
-cp -f /vagrant/deepdream.conf /etc/init/
-stop deepdream
-start deepdream
+echo "---------------" &>> /vagrant/log/provision.log
+date &>> /vagrant/log/provision.log
+mkdir -p /home/vagrant/sasi &>> /vagrant/log/provision.log
+cp -f /vagrant/deepdream.ipynb /home/vagrant/sasi/ &>> /vagrant/log/provision.log
+chown -R vagrant:vagrant /home/vagrant &>> /vagrant/log/provision.log
+cp -f /vagrant/deepdream.conf /etc/init/ &>> /vagrant/log/provision.log
+initctl reload deepdream || start deepdream &>> /vagrant/log/provision.log
